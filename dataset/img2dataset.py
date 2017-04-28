d53f65399d184d6dad3cd85181050ac7fc559ee1
Syee <blue2856@naver.com>
2017-04-28 18:19:00 +0900

    6th + : data and data gen code

---
+++ dataset/img2dataset.py
@@ -0,0 +1,43 @@
+﻿import numpy as np
+import glob
+import os
+from scipy.misc import imread, imresize, imshow
+import matplotlib.pyplot as plt
+
+
+# 이미지 파일의 이름을 분리해서 저장.
+cwd = os.getcwd()
+imgsize = [96, 128]
+
+file_list = glob.glob(cwd + '/image/*.jpg')
+speed = [] # output data (speed, angle ...)
+angle = []
+img = [] # input data (image)
+
+
+file_name = "data.npz"
+
+# 폴더안의 모든 image파일 이름에서 정보 추출하고 gray scale로 변환
+for file in file_list:
+    file_path = file
+    currimg =  imread(file_path)
+    file = os.path.basename(file)
+    file_ = os.path.splitext(file)[0]
+    _, s, a = file_.split('_')
+    speed.append(int(s))
+    angle.append(int(a))
+    currimg = currimg[48:96]
+    img.append(currimg)
+
+
+print (len(speed))
+print(len(angle))
+np.savez(file_name, image = img, speed = speed, angle = angle, dtype='int')
+
+Car_data = np.load('data.npz')
+raw_img = Car_data['image']
+trainy = Car_data['angle']
+print ("------------------------------")
+print (raw_img.shape)
+print (trainy)
+print (trainy.shape)
---
+++ dataset/merge.py
@@ -0,0 +1,17 @@
+﻿import numpy as np
+
+
+d = np.load("data.npz")
+d2 = np.load("data2.npz")
+
+
+img = list(d['image'])+list(d2['image'])
+spd = list(d['speed'])+list(d2['speed'])
+ang = list(d['angle'])+list(d2['angle'])
+
+print(len(img))
+print(len(spd))
+print(len(ang))
+
+
+np.savez("new_data.npz",image= img, speed = spd, angle=ang )
